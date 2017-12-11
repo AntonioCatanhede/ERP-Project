84bfb6b97a06f1c49a74ba842f1b99102ae90cc9
Farago Bence <semmiertelme13@gmail.com>
2017-12-11 11:15:54 +0100

    Merged the files

--- sales/sales.py
+++ sales/sales.py
@@ -1,4 +1,3 @@
-week2
 # data structure:
 # id: string
 #     Unique and random generated (at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter)
@@ -8,7 +7,6 @@ week2
 # day: number
 # year: number
 # month,year and day combined gives the date the sale was made
-# customer_id: string, id from the crm
 
 # importing everything you need
 import os
@@ -18,6 +16,9 @@ import ui
 import data_manager
 # common module
 import common
+
+the_list = data_manager.get_table_from_file("sales/sales.csv")
+menu_list = ["title:", "price:", "month:", "day:", "year:"]
 
 
 def start_module():
@@ -30,9 +31,40 @@ def start_module():
         None
     """
 
-    # your code
-
-    pass
+    ui.print_menu("Sales", ["show_table", "add", "remove", "update",
+                            "lowest price item", "get item between date"], "Go back to main menu")
+    menu_choose_list = ui.get_inputs(["Choose: "], "")
+    menu_choose = int(menu_choose_list[0])
+    if menu_choose == 1:
+        show_table(the_list)
+        menu_list.remove("id")
+        start_module()
+    elif menu_choose == 2:
+        add(the_list)
+        start_module()
+    elif menu_choose == 3:
+        id = ui.get_inputs(["ID: "], "Please enter an id: ")
+        id = id[0]
+        remove(the_list, id)
+        start_module()
+    elif menu_choose == 4:
+        id = ui.get_inputs(["ID: "], "Please enter an id: ")
+        id = id[0]
+        update(the_list, menu_list, id)
+        start_module()
+    elif menu_choose == 5:
+        ui.print_result(get_lowest_price_item_id(the_list), "was sold for the lowest price")
+        start_module()
+    elif menu_choose == 6:
+        dates = ui.get_inputs(["month from", "day from", "year from", "month to", "day to",
+                               "year to"], "enter -date from and -date to want to observ")
+        ui.print_result(get_items_sold_between(the_list, dates[0], dates[1], dates[2], dates[3], dates[4], dates[5]), "item(s) between the 2 given date")
+        start_module()
+    elif menu_choose == 0:
+        data_manager.write_table_to_file("sales/sales.csv", the_list)
+        return
+    else:
+        raise KeyError("There is no such options")
 
 
 def show_table(table):
@@ -46,9 +78,8 @@ def show_table(table):
         None
     """
 
-    # your code
-
-    pass
+    menu_list.insert(0, "id")
+    ui.print_table(table, menu_list)
 
 
 def add(table):
@@ -62,9 +93,24 @@ def add(table):
         Table with a new record
     """
 
-    # your code
+    while True:
+        returnable_list = common.common_add(table, menu_list)
 
-    return table
+        if (returnable_list[-1][1]).isdigit() == False and \
+            (returnable_list[-1][2]).isdigit() and \
+            (returnable_list[-1][3]).isdigit() and \
+            int((returnable_list[-1][3])) > 0 and \
+            int((returnable_list[-1][3])) < 13 and \
+            (returnable_list[-1][4]).isdigit() and\
+            int((returnable_list[-1][4])) > 0 and \
+            int((returnable_list[-1][4])) < 32 and \
+            (returnable_list[-1][5]).isdigit() and \
+            int((returnable_list[-1][5])) < 3000 and \
+                int((returnable_list[-1][5])) > 0:
+            return returnable_list
+        else:
+            the_list.remove(the_list[-1])
+            ui.print_error_message("\nYou entered wrong inputs\n")
 
 
 def remove(table, id_):
@@ -79,12 +125,10 @@ def remove(table, id_):
         Table without specified record.
     """
 
-    # your code
-
-    return table
+    return common.common_remove(table, id_)
 
 
-def update(table, id_):
+def update(table, list, id_):
     """
     Updates specified record in the table. Ask users for new data.
 
@@ -96,9 +140,27 @@ def update(table, id_):
         table with updated record
     """
 
-    # your code
+    while True:
+        returnable_list = common.common_update(table, list, id_)
 
-    return table
+        for item in returnable_list:
+            if id_ in item:
+                comparable_list = item
+
+        if (comparable_list[1]).isdigit() == False and \
+            (comparable_list[2]).isdigit() and \
+            (comparable_list[3]).isdigit() and \
+            int((comparable_list[3])) > 0 and \
+            int((comparable_list[3])) < 13 and \
+            (comparable_list[4]).isdigit() and\
+            int((comparable_list[4])) > 0 and \
+            int((comparable_list[4])) < 32 and \
+            (comparable_list[5]).isdigit() and \
+            int((comparable_list[5])) < 3000 and \
+                int((comparable_list[5])) > 0:
+            return returnable_list
+        else:
+            ui.print_error_message("\nYou entered wrong inputs\n")
 
 
 # special functions:
@@ -108,19 +170,32 @@ def update(table, id_):
 # return type: string (id)
 # if there are more than one with the lowest price, return the first by descending alphabetical order
 def get_lowest_price_item_id(table):
-
-    # your code
-
-    pass
+    prices = []
+    name_list = []
+    for line in table:
+        prices.append(line[2])
+    min_price = min(prices)
+    for line in table:
+        if min_price in line:
+            name_list.append(line[1])
+    name = min(name_list)
+    for line in table:
+        if name in line:
+            return line[0]
 
 
 # the question: Which items are sold between two given dates ? (from_date < sale_date < to_date)
 # return type: list of lists (the filtered table)
 def get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to):
 
-    # your code
-
-    pass
+    returnable_list = []
+    datefrom = int(str(year_from) + str(month_from) + str(day_from))
+    dateto = int(str(year_to) + str(month_to) + str(day_to))
+    for line in table:
+        date = int(str(line[5]) + str(line[3]) + str(line[4]))
+        if date > datefrom and date < dateto:
+            returnable_list.append(line)
+    return returnable_list
 
 # functions supports data abalyser
 # --------------------------------
@@ -365,203 +440,3 @@ def get_num_of_sales_per_customer_ids_from_table(table):
     # your code
 
     pass
-
-# data structure:
-# id: string
-#     Unique and random generated (at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter)
-# title: string
-# price: number (the actual sale price in $)
-# month: number
-# day: number
-# year: number
-# month,year and day combined gives the date the sale was made
-
-# importing everything you need
-import os
-# User interface module
-import ui
-# data manager module
-import data_manager
-# common module
-import common
-
-the_list = data_manager.get_table_from_file("sales/sales.csv")
-menu_list = ["title:", "price:", "month:", "day:", "year:"]
-
-
-def start_module():
-    """
-    Starts this module and displays its menu.
-    User can access default special features from here.
-    User can go back to main menu from here.
-
-    Returns:
-        None
-    """
-
-    ui.print_menu("Sales", ["show_table", "add", "remove", "update",
-                            "lowest price item", "get item between date"], "Go back to main menu")
-    menu_choose_list = ui.get_inputs(["Choose: "], "")
-    menu_choose = int(menu_choose_list[0])
-    if menu_choose == 1:
-        show_table(the_list)
-        menu_list.remove("id")
-        start_module()
-    elif menu_choose == 2:
-        add(the_list)
-        start_module()
-    elif menu_choose == 3:
-        id = ui.get_inputs(["ID: "], "Please enter an id: ")
-        id = id[0]
-        remove(the_list, id)
-        start_module()
-    elif menu_choose == 4:
-        id = ui.get_inputs(["ID: "], "Please enter an id: ")
-        id = id[0]
-        update(the_list, menu_list, id)
-        start_module()
-    elif menu_choose == 5:
-        ui.print_result(get_lowest_price_item_id(the_list), "was sold for the lowest price")
-        start_module()
-    elif menu_choose == 6:
-        dates = ui.get_inputs(["month from", "day from", "year from", "month to", "day to",
-                               "year to"], "enter -date from and -date to want to observ")
-        ui.print_result(get_items_sold_between(the_list, dates[0], dates[1], dates[2], dates[3], dates[4], dates[5]), "item(s) between the 2 given date")
-        start_module()
-    elif menu_choose == 0:
-        data_manager.write_table_to_file("sales/sales.csv", the_list)
-        return
-    else:
-        raise KeyError("There is no such options")
-
-
-def show_table(table):
-    """
-    Display a table
-
-    Args:
-        table: list of lists to be displayed.
-
-    Returns:
-        None
-    """
-
-    menu_list.insert(0, "id")
-    ui.print_table(table, menu_list)
-
-
-def add(table):
-    """
-    Asks user for input and adds it into the table.
-
-    Args:
-        table: table to add new record to
-
-    Returns:
-        Table with a new record
-    """
-
-    while True:
-        returnable_list = common.common_add(table, menu_list)
-
-        if (returnable_list[-1][1]).isdigit() == False and \
-            (returnable_list[-1][2]).isdigit() and \
-            (returnable_list[-1][3]).isdigit() and \
-            int((returnable_list[-1][3])) > 0 and \
-            int((returnable_list[-1][3])) < 13 and \
-            (returnable_list[-1][4]).isdigit() and\
-            int((returnable_list[-1][4])) > 0 and \
-            int((returnable_list[-1][4])) < 32 and \
-            (returnable_list[-1][5]).isdigit() and \
-            int((returnable_list[-1][5])) < 3000 and \
-                int((returnable_list[-1][5])) > 0:
-            return returnable_list
-        else:
-            the_list.remove(the_list[-1])
-            ui.print_error_message("\nYou entered wrong inputs\n")
-
-
-def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
-
-    Args:
-        table: table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        Table without specified record.
-    """
-
-    return common.common_remove(table, id_)
-
-
-def update(table, list, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
-
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        table with updated record
-    """
-
-    while True:
-        returnable_list = common.common_update(table, list, id_)
-
-        for item in returnable_list:
-            if id_ in item:
-                comparable_list = item
-
-        if (comparable_list[1]).isdigit() == False and \
-            (comparable_list[2]).isdigit() and \
-            (comparable_list[3]).isdigit() and \
-            int((comparable_list[3])) > 0 and \
-            int((comparable_list[3])) < 13 and \
-            (comparable_list[4]).isdigit() and\
-            int((comparable_list[4])) > 0 and \
-            int((comparable_list[4])) < 32 and \
-            (comparable_list[5]).isdigit() and \
-            int((comparable_list[5])) < 3000 and \
-                int((comparable_list[5])) > 0:
-            return returnable_list
-        else:
-            ui.print_error_message("\nYou entered wrong inputs\n")
-
-
-# special functions:
-# ------------------
-
-# the question: What is the id of the item that was sold for the lowest price ?
-# return type: string (id)
-# if there are more than one with the lowest price, return the first by descending alphabetical order
-def get_lowest_price_item_id(table):
-    prices = []
-    name_list = []
-    for line in table:
-        prices.append(line[2])
-    min_price = min(prices)
-    for line in table:
-        if min_price in line:
-            name_list.append(line[1])
-    name = min(name_list)
-    for line in table:
-        if name in line:
-            return line[0]
-
-
-# the question: Which items are sold between two given dates ? (from_date < sale_date < to_date)
-# return type: list of lists (the filtered table)
-def get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to):
-
-    returnable_list = []
-    datefrom = int(str(year_from) + str(month_from) + str(day_from))
-    dateto = int(str(year_to) + str(month_to) + str(day_to))
-    for line in table:
-        date = int(str(line[5]) + str(line[3]) + str(line[4]))
-        if date > datefrom and date < dateto:
-            returnable_list.append(line)
-    return returnable_list
-master
